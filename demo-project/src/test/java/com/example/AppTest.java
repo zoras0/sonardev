8daec7c35d9351ea932baabfc0dee8f73f745175
zoras0 <168116990+zoras0@users.noreply.github.com>
2026-03-23 10:08:23 +0000

    sad

--- demo-project/src/test/java/com/example/AppTest.java
+++ demo-project/src/test/java/com/example/AppTest.java
@@ -1,13 +1,38 @@
 package com.example;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 /**
- * Hello world!
- *
+ * Unit test for simple App.
  */
-public class App 
+public class AppTest 
+    extends TestCase
 {
-    public static void main( String[] args )
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
     {
-        System.out.println( "Hello World!" );
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
